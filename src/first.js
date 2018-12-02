de4108da9ad9af0542c8b411a89f7da650c10d63
Jerry Li <xiaoroyg@163.com>
2018-12-02 22:41:31 +0800

    Initial Commit.

---
+++ src/first.js
@@ -0,0 +1,29 @@
+//variable is a symbolic name for a value.
+var x;
+x = 0;
+x = 1;
+x = 0.01;
+x = "Hello Wolrd";
+x = 'Java Script';
+x = true;
+x = false;
+x = null;
+x= undefined;
+
+// JavaScript's most important data type is the object.
+// An object is a collection of name/value pairs, or a string to value map.
+var book = {
+    topic: "JavaScript",
+    author: "NoOne"
+}
+
+book.author
+book[topic]
+book.pages = 234
+book.conent = {}
+
+// JavaScript also supports arrays (numerically indexed lists) of values
+var primes = [2, 3, 5, 7]
+primes[0]
+primes.length
+var emptyArray = []
