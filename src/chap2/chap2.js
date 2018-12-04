30276e478874b9a59dc11e8d1c3db04f71f85ef6
Jerry Li <xiaoroyg@163.com>
2018-12-04 23:42:42 +0800

    Learned unicode escape sequences.

---
+++ src/chap2/chap2.js
@@ -0,0 +1,3 @@
+//2.1.3 Unicode Escape Sequences
+
+var result = "café" === "caf\u00e9" // true
--- src/first.js
+++ src/first.js
@@ -69,3 +69,15 @@ function abs(number){
     } 
     return result;
 }
+
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Point.prototype.r = function() {
+    return Math.sqrt(
+        this.x * this.x +
+        this.y + this.y
+        );
+};
