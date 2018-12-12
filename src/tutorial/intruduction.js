23795fe241a3f9417c7e0fe09198819f9e0428be
Jerry Li <xiaoroyg@163.com>
2018-12-12 23:47:51 +0800

    Learned js let.

---
+++ src/tutorial/intruduction.js
@@ -0,0 +1,22 @@
+//Number
+Math.sin(3.5)
+var circumference = 2 * Math.PI * 3
+var count =  + '42' // 42
+var countA = + '010' //10
+var countB = + '0x10' // 16
+var notANumber = parseInt('hello', 10)
+var stillNotANumber = notANumber + 5
+var aInfinity = 1 / 0
+var aNegativeInfinity = -1 / 0  
+
+//String
+'hello'.length //5
+'hello'.toUpperCase() //'Hello'
+
+Boolean('') //false
+Boolean(234) // true
+
+//Variables
+for(let index = 0; index < 5; index ++){
+    //do something
+}
