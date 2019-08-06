b9a4cfff12f3275553cc21f2cbbad5f785d18daf
Jerry Li <xiaoroyg@163.com>
2019-08-06 23:29:28 +0800

    Learned numbers.

---
+++ src/chap3/numbers.js
@@ -0,0 +1,16 @@
+var aInfinite = 1 / 0;
+console.log(aInfinite);
+
+var theNaN = 0 / 0;
+console.log(theNaN);
+var notEquals = theNaN == theNaN;
+console.log(notEquals);
+console.log(isNaN(theNaN));
+
+var zero = 0;
+var negativeZero = 0;
+var zeroEquals = (zero === negativeZero); // true: zero and negative zero are equal
+var infiniteNotEqual = (1 / zero) === (1 / negativeZero);// false: infinity and -infinity are not equal
+
+var now = new Date()
+console.log(now)
