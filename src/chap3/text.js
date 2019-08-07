8407b9c3ee381c9f8397768dab64c2c2d03beb48
Jerry Li <xiaoroyg@163.com>
2019-08-08 00:52:54 +0800

    Learned text.

---
+++ src/chap3/text.js
@@ -0,0 +1,12 @@
+var first = 'key = "index"'
+var second = "You've pick a prospective partner."
+
+var ignore = '\#' // is #
+console.log(ignore)
+
+var hello = "hello"
+var world = "wordl"
+var message = hello + world
+
+var name = "Huawei"
+var first = name[0] 
