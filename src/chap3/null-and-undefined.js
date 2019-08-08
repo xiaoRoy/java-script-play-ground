16f905adb16f826399fde04fec8a7e279a205823
Jerry Li <xiaoroyg@163.com>
2019-08-09 00:16:22 +0800

    Learned null and undefined.

---
+++ src/chap3/null-and-undefined.js
@@ -0,0 +1,12 @@
+var typeOfNull = typeof(null); //=> object
+console.log(typeOfNull); 
+
+var one;
+var tyepOfUndefined = typeof(one);
+console.log(tyepOfUndefined); // => undefined
+
+var isEqual = (null == undefined); // => true
+console.log(isEqual);
+
+var isNotEqual = (null === undefined); // => false
+console.log(isNotEqual);
