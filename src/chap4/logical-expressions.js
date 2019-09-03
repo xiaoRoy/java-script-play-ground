ea8bcb9e9d784ecc1490f10a5987fd2e5875e87e
Jerry Li <xiaoroyg@163.com>
2019-09-03 23:33:29 +0800

    Learned logical expressions.

---
+++ src/chap4/logical-expressions.js
@@ -0,0 +1,12 @@
+/**
+ * 4.10.3 Logical NOT (!)
+ */
+
+ // false 
+
+ var first = !!undefined;
+ var second = !!null;
+ var third = !!"";
+ var fourth = !!0;
+ var fifth = !!-0;
+ var sixth = !!NaN;
