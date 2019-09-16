ecf29e413f3051f3fbf0d67f87c2bd734bb8c852
Jerry Li <xiaoroyg@163.com>
2019-09-16 23:10:57 +0800

    Learned delete properties.

---
+++ src/chap6/deleting-properties.js
@@ -0,0 +1,33 @@
+/**
+ * 6.3 Deleting propterties
+ */
+
+var dog = {
+    name: "Dog",
+    age: "2",
+    price: 44.0
+}
+
+delete dog.price; // Delete x, and return true.
+delete dog.price; // Do nothing(price doesn't exist), and return true.
+delete dog.toString(); // Do noting(toString is inherited property), and return true.
+delete 44; // Nonsense, but evaluates to true.
+
+
+delete Object.prototype; // => false in non-strict mode
+
+var employee = {};
+Object.defineProperty(employee, 'name', { configurable: false });
+var result = delete employee.name;
+console.log(result); // => false in non-strict mode
+
+function testDeletingProperties() {
+    'use strict'
+    // delete Object.prototype;// TypeError in strict mode
+
+    Object.defineProperty(employee, 'age', { configurable: false });
+    delete employee.age; // TypeError in strict mode
+}
+
+testDeletingProperties();
+
