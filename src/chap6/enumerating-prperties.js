1c7ed191f4533cd6c45180183e1a5787c93d0429
Jerry Li <xiaoroyg@163.com>
2019-09-19 22:32:03 +0800

    Learned enumerate the properites.

--- src/chap6/enumerating-prperties.js
+++ src/chap6/enumerating-prperties.js
@@ -2,19 +2,72 @@
  * 6.5 Enumerating Properties
  */
 
-var point = {
-    x: 1.22,
-    y: 2.24,
-    z: 31.4
+
+function enumeratePoint() {
+    var point = {
+        x: 1.22,
+        y: 2.24,
+        z: 31.4
+    }
+
+    for (propertyInPoint in point) {
+        //propertyInPoint is a global property
+        console.log(point[propertyInPoint]);
+    }
 }
 
-for (property in point) {
-    //propety is a global property
-    console.log(point[property]);
+enumeratePoint();
+console.log(propertyInPoint);
+
+function extend(one, anohter) {
+    for (var property in anohter) {
+        one[property] = anohter[property];
+    }
+    return one;
 }
 
-console.log(property);
+function merge(one, anohter) {
+    for (var property in anohter) {
+        if(one.hasOwnPropert(property)) {
+            continue;
+        }
+        one[property] = anohter[property];
+    }
+    return one;
+}
 
-function extend() {
-    
+function restrict(one, anohter) {
+    for(var property in one) {
+        if(!(property in anohter)) {
+            delete one[property];
+        }
+    }
+    return one;
+}
+
+function substract(one, another) {
+    for(var property in another) {
+        delete one[property];
+    }
+    return one;
+}
+
+function union(one, another) {
+    return extend(extend({}, one), anohter);
+}
+
+function intersection(one, anohter) {
+    return restrict(restrict({}, one), another);
+}
+
+function keys(one) {
+    if(typeof one !== "object") {
+        throw TypeError();
+    }
+    var result = [];
+    for(var property in one) {
+        if(one.hasOwnPropert(property)) {
+
+        }
+    }
 }
