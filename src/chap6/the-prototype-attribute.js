e6eef625d48674ec1e3c42d92b92e8beba07322d
Jerry Li <xiaoroyg@163.com>
2019-09-28 23:05:27 +0800

    Learned clas attribute.

--- src/chap6/the-prototype-attribute.js
+++ src/chap6/the-prototype-attribute.js
@@ -1,11 +1,31 @@
+/**
+ * 6.8.1 The prototype Attribute
+ */
 var point = {
     x: 1,
     y: 3.14
-}
+};
 
 var anotherPoint = Object.create(point);
-var isPrototype = point.isPrototypeOf(anotherPoint) // => true
+var isPrototype = point.isPrototypeOf(anotherPoint); // => true
 console.log(isPrototype);
-var isObjectPrototype = Object.prototype.isPrototypeOf(anotherPoint) // => true
+var isObjectPrototype = Object.prototype.isPrototypeOf(anotherPoint); // => true
 console.log(isObjectPrototype);
-console.log(Object.getPrototypeOf(anotherPoint))
+console.log(Object.getPrototypeOf(anotherPoint));
+
+/**
+ * 6.8.2 The class Attribute
+ */
+
+ function classOf(object) {
+    var result;
+    if(object === null) {
+        result = "Null";
+    }  else if (object === undefined) {
+        result = "Undefined";
+    } else {
+        result = Object.prototype.toString.call(object).slice(8, -1);
+    }
+    return result;
+ }
+ console.log(classOf(point));
