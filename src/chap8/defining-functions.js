63e8834c33c309d38e7449f17f7edac781cd9116
Jerry Li <xiaoroyg@163.com>
2019-10-28 22:14:26 +0800

    Learned define function.

---
+++ src/chap8/defining-functions.js
@@ -0,0 +1,34 @@
+function pirntProperties(target) {
+    for (var property in target) {
+        console.log(property + ":" + target[property]);
+    }
+}
+
+function calculateDistance(pointA, pointB) {
+    var dx = pointA.x - pointB.x;
+    console.log("dx:" + dx);
+    var dy = pointA.y - pointB.y;
+    console.log("dy:" + dy);
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
+var pointA = {
+    x: 3.14,
+    y: 44
+}
+
+var distance = calculateDistance(pointA, {
+    x: 2.12,
+    y: 14
+});
+console.log(distance);
+
+function factorial(number) {
+    if(number <= 1) return 1
+    return number * factorial(number -1); 
+}
+
+var square = function(number) {return number * number}
+
+var tenSquare = (function(number){return number * number})(10);
+console.log(tenSquare);
