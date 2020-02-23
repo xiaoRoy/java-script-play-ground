73c3f84b2620e26d8a10c6d6a0ae7e8eeaa86386
Jerry Li <xiaoroyg@163.com>
2020-02-23 08:27:57 +0800

    Learned Java-Style Classes in JavaScript

--- src/chap9/classes-and-prototypes.js
+++ src/chap9/classes-and-prototypes.js
@@ -93,15 +93,20 @@ function Rectangle(width, height) {
     this.width = width;
     this.height = height;
 }
+Rectangle.name = "Rectangle";
 
 Rectangle.prototype = {
-    constructor: Rectangle,
-    width: 0,
-    height: 0,
+    // constructor: Rectangle,
+    // width: 0,
+    // height: 0,
+    // name: "",
     area: function () {
         return this.width * this.height;
     }
 }
+
+var rectangleB = new Rectangle(4, 5);
+console.log("RectangleB:" + Rectangle.name);
 
 function Square(width) {
     this.width = width
@@ -112,6 +117,7 @@ Square.prototype = Rectangle.prototype
 var square = new Square(4);
 var area = square.area();
 console.log(area);
+console.log(square.name);
 
 var isRectangle = square instanceof Rectangle
 console.log(isRectangle); // => true
@@ -135,6 +141,83 @@ console.log(Rectangle.prototype.constructor); // => function Rectangle(width, he
 function Product(id, name) { }
 console.log(Product.prototype.constructor); // => function Product(id, name) { … }
 
-function Cup(id) {}
+function Cup(id) { }
 Cup.prototype = {}
 console.log(Cup.prototype.constructor); // => function Object() { … }
+
+
+/**
+ * 9.3 Java-Style Classes in JavaScript
+ */
+function extend(one, another) {
+    for (property in another) {
+        console.log("propertyName:" + property)
+        one[property] = another[property];
+    }
+    return one;
+}
+
+
+function defineSimpleClass(constructor, methods, statics) {
+    if (methods) extend(constructor.prototype, methods);
+    if (statics) extend(constructor, statics);
+    return constructor;
+}
+
+var SimpleRange = defineSimpleClass(
+    function (from, to) { this.from = from, this.to = to },
+    {
+        includes: function (input) { return input >= this.from && this.to <= input; },
+        toString: function () { return this.from + "..." + this.to; }
+    },
+    { upto: function (to) { return new SimpleRange(0, to); } }
+);
+
+function Complex(real, imaginary) {
+    if (isNaN(real) || isNaN(imaginary)) throw new TypeError();
+    this.real = real;
+    this.imaginary = imaginary;
+}
+
+Complex.prototype.add = function (another) {
+    return new Complex(this.real + another.real, this.imaginary + another.imaginary);
+}
+
+Complex.prototype.negative = function () {
+    return new Complex(-this.real, -this.imaginary);
+}
+
+Complex.prototype.toString = function () {
+    return "{" + this.real + "," + this.imaginary + "}";
+}
+
+Complex.prototype.equals = function (another) {
+    return another != null &&
+        this.constructor === Complex &&
+        this.real === another.real && this.imaginary === another.real;
+}
+
+Complex.ZERO = new Complex(0, 0);
+Complex.ONE = new Complex(1, 0);
+Complex.I = new Complex(0, 1);
+Complex._format = /^\{([^,]+),([^}]+)\}$/;
+
+Complex.pares = function (input) {
+    try {
+        var result = Complex._format.exec(input);
+        console.log(result);
+        return new Complex(parseFloat(result[1]), parseFloat(result[2]));
+    } catch (exception) {
+        throw new TypeError("Can't parse '" + input + "' as a complex number");
+    }
+}
+
+var complexA = new Complex(2, 3);
+var complexB = new Complex(complexA.real, complexA.imaginary);
+console.log(complexA.add(complexB).toString());
+
+var reusltA = Complex
+    .pares("{4,4}")
+    .add(new Complex(4, 4).negative())
+    .equals(Complex.ZERO);
+console.log("Complex ResultB:" + reusltA);
