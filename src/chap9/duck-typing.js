1c2f597c892a4cf529bc78909a663fb6c9b181bd
Jerry Li <xiaoroyg@163.com>
2020-02-28 08:29:17 +0800

    Added check quack

--- src/chap9/duck-typing.js
+++ src/chap9/duck-typing.js
@@ -1,23 +1,23 @@
 /**
- * 
+ * 9.5.4 Duck-Typing
  */
 
 function TriAthlete() {
 
 }
 
-TriAthlete.prototype.run = function() {
+TriAthlete.prototype.run = function () {
     console.log("I could run.");
 }
-TriAthlete.prototype.swim = function() {
+TriAthlete.prototype.swim = function () {
     console.log("I could swim.");
 }
-TriAthlete.prototype.bike = function() {
+TriAthlete.prototype.bike = function () {
     console.log("I could bike.");
 }
 
 function joinTriathlon(who) {
-    if(who.run && who.swim && who.bike) {
+    if (who.run && who.swim && who.bike) {
         console.log("Allow to join the Triathlon.");
     } else {
         console.log("Not allow to join the Triathlon.");
@@ -27,24 +27,63 @@ function joinTriathlon(who) {
 joinTriathlon(new TriAthlete());
 
 joinTriathlon({
-    run: function(){},
-    swim: function(){},
-    bike: function(){}
+    run: function () { },
+    swim: function () { },
+    bike: function () { }
 })
 
 joinTriathlon({
-    run: function() {}
+    run: function () { }
 });
 
 function quack(object, /*...*/) {
     var argumentCount = arguments.length;
-    for(var index = 1; index < argumentCount; index ++) {
-        var argumment = arguments[i];
+    for (var index = 1; index < argumentCount; index++) {
+        var argument = arguments[index];
+        switch (typeof argument) {
+            case 'string':
+                if (typeof object[argument] !== "function") {
+                    return false;
+                }
+                continue;
+            case 'function':
+                argument = argument.prototype;
+            //fall through to next case
+            case 'object':
+                for (var what in argument) {
+                    // if(typeof argument[what] !== 'function') continue;
+                    // if(typeof object[what] !== 'function') return false;  
+                    if (typeof argument[what] === 'function' && typeof object[what] !== 'function') {
+                        return false
+                    }
+                }
+        }
+    }
+    return true;
+}
+
+var bird = {
+    fly: function () {
+
+    },
+
+    eat: function () {
+
     }
 }
 
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Point.prototype.name = function() {};
+
+var quackResult = quack(bird, "fly", { eat: function(){ } }, Point);
+console.log("quackResult:" + quackResult);
+
+
 var what = {
-    0:4,
-    "1":3
+    0: 4,
+    "1": 3
 }
 console.log(what[1]);
