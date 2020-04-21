cc7b49dc909eff0d4c0d2108fd1b6fc83532b815
Jerry Li <xiaoroyg@163.com>
2020-04-21 08:05:03 +0800

    Learned constructor.

--- src/chap9/private-state.js
+++ src/chap9/private-state.js
@@ -6,24 +6,24 @@ function Range(from, to) {
 Range.prototype = {
     constructor: Range,
 
-    includes: function(value) {
+    includes: function (value) {
         return this.from() <= value && value <= this.to();
     },
 
-    forEach: function(operation) {
-        for(var element = Math.ceil(this.from(), max = this.to()); element < max; element ++) {
+    forEach: function (operation) {
+        for (var element = Math.ceil(this.from(), max = this.to()); element < max; element++) {
             operation(element);
         }
     },
 
-    toString: function() {
+    toString: function () {
         return "(" + this.from() + "..." + this.to() + ")";
     }
 };
 
 var range = new Range(0, 44);
 console.log(range.toString());
-range.forEach(function(element) {
+range.forEach(function (element) {
     if (element == 43) {
         console.log(43);
     }
@@ -35,10 +35,25 @@ console.log(is45InRagne);
 
 function isArrayLike(object) {
     var length = object.length;
-    return object && 
-            typeof object === "object"
-            isFinite(length) && 
-            length > 0 &&
-            length === Math.floor(length) &&
-            length < Math.pow(2, 32)
+    return object &&
+        typeof object === "object"
+    isFinite(length) &&
+        length > 0 &&
+        length === Math.floor(length) &&
+        length < Math.pow(2, 32)
+}
+
+function SetWithArray() {
+    this.values = {};
+    this.count = 0;
+    var argumentsLength = arguments.length;
+    if (argumentsLength == 1 && isArrayLike(arguments[0])) {
+        this.add.apply(this, arguments[0]);
+    } else if(argumentsLength > 0) {
+        this.add.apply(this, arguments);
+    }
+}
+
+SetWithArray.prototype.add = function() {
+    // place holder method
 }
