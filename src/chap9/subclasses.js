615eb1ea5bb75e7f646e3b7ebde8c5d3000f0312
Jerry Li <xiaoroyg@163.com>
2020-04-24 09:04:50 +0800

    Learned subclass

--- src/chap9/subclasses.js
+++ src/chap9/subclasses.js
@@ -2,3 +2,13 @@ import { Set } from "./object-oriented.js"
 
 var setA = new Set(1, 4, 44);
 console.log(setA.toString())
+
+function Animal() {}
+
+function Cat() {}
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat
+var catA = new Cat();
+var isCatAnimal = catA instanceof Animal
+console.log(isCatAnimal);
