502a9c1dc0911ce786abe19e2cf54df0d2d733e5
Jerry Li <xiaoroyg@163.com>
2020-04-26 23:15:26 +0800

    Learned subclass.

--- src/chap9/subclasses.js
+++ src/chap9/subclasses.js
@@ -12,3 +12,27 @@ Cat.prototype.constructor = Cat
 var catA = new Cat();
 var isCatAnimal = catA instanceof Animal
 console.log(isCatAnimal);
+
+function inherit(from) {
+    if (from == null) throw TypeError();
+    if (Object.create) {
+        return Object.create(from);
+    }
+    var typeOfFrom = typeof from
+    if (typeOfFrom !== "object" && typeOfFrom !=="function") throw TypeError(); 
+    function Dummy() {};
+    Dummy.prototype = from;
+    return new Dummy();
+}
+
+function defineSubClass(superClass, constructor, methods, statics) {
+    constructor.prototype = inherit(superClass.prototype);
+    constructor.prototype.constructor = constructor;
+}
+
+function copyPropertiest(from, to) {
+    for(var propertyName in from) {
+        to[propertyName] = from[propertyName];
+    }
+    return to;
+}
