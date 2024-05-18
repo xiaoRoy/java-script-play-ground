42b36582b5f35e9217274e1f2bf47000517d1e2e
xiaoRoy <xiaoroyg@163.com>
2024-05-18 23:51:39 +0800

    Learned screen object.

---
+++ src/professional-javascript/chap12/scripts/scrren-object.js
@@ -0,0 +1,7 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log(`width:${window.screen.width}`);
+    console.log(`height:${window.screen.height}`);
+    console.log(`pixelDepth:${window.screen.pixelDepth}`);
+});
